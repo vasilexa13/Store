af2bc213c5e2292be6fdbd39ddb9d1ca20bccbcb
vasilexa13 <vasilexa@gmail.com>
2024-10-26 22:56:07 +0300

    created sorting by rating

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -10,10 +10,16 @@ export class ProductsService {
     this.db = getDb()
     }
 
-    async findAll(itemGroup: string) {
-        // console.log(this.db.collection('products').findAll({'itemGroup': 'phones'}))
+    async findAll(itemGroup: string, sortBy?:string) {
         try {
-            return await this.db.collection('products').find({'itemGroup': itemGroup.toString()}).toArray();
+            // return await this.db.collection('products').find({'itemGroup': itemGroup.toString()}).toArray();
+
+            if (sortBy === 'rating'){
+                return await this.db.collection('products').find({'itemGroup': itemGroup.toString()}).sort({'rating':-1}).toArray();
+            }
+            else{
+                return await this.db.collection('products').find({'itemGroup': itemGroup.toString()}).toArray();
+            }
         }
         catch(err) {
             throw new NotFoundException(`Item with ${itemGroup} not found`);
